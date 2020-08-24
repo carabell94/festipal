61e8e3c597fd30dc723f4f40d44e50a419ebff7c
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-24 15:22:07 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :festival, only: [:index, :show] do
+    resource :schedule, only: [:index, :create, :update, :destroy]
+  end
 end
