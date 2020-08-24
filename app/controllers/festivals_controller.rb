9129010464a7edd1339f4ea1e4a299cbbf3a2b72
Cara Bell <carabell1994@aim.com>
2020-08-24 16:38:01 +0100

    controllers

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,3 +1,11 @@
 class FestivalsController < ApplicationController
 
+def index
+  @festivals = Festival.all
+end
+
+def show
+  @festival = Festival.find(params[:id])
+end
+
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,13 +1,38 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Schedule.all
   end
 
-  def create
-  end
+  # def create
+  #   @schedule = Schedule.new(strong_params)
+  #   @schedule.user = current_user
+  #   @festival = Festival.find(params[:festival_id])
+  #   @festival.schedule = @schedule
+  #   if @schedule.save!
+  #     redirect_to root_path # change this path
+  #   end
+  # end
 
-  def update
-  end
+  # def update
+  #   find_by_id
+  #   if @schedule.update(strong_params)
+  #     redirect_to root_path # change this path
+  #   end
+  # end
 
-  def destroy
-  end
+  # def destroy
+  #   find_by_id
+  #   @schedule.destroy
+  #   redirect_to root_path # change this path
+  # end
+
+  # private
+
+  # def strong_params
+  #   params.require(:schedule).permit(:festival_id, :day, :start_time, :end_time, :artist, :genre, :stage)
+  # end
+
+  # def find_by_id
+  #   @schedule = Schedule.find(params[:id])
+  # end
 end
