d97f2541cf2bb2b314a2191029d280704a1e6374
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-25 17:10:17 +0100

    seed completed

--- db/migrate/20200824135128_create_schedules.rb
+++ db/migrate/20200824135128_create_schedules.rb
@@ -3,8 +3,8 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
     create_table :schedules do |t|
       t.references :festival, null: false, foreign_key: true
       t.string :day
-      t.datetime :start_time
-      t.datetime :end_time
+      t.integer :start_time
+      t.integer :end_time
       t.string :artist
       t.string :stage
       t.string :genre
