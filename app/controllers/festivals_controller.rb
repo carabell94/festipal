fa4a23442b80f5b89e23626f68ead741fa05083f
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-26 12:42:31 +0100

    creating tabs

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,11 +1,16 @@
 class FestivalsController < ApplicationController
-
 def index
   @festivals = Festival.all
+  # @search = params['search']
+  # if @search.present?
+  #   @name = @search['name']
+  #   # @festivals = Festival.where(name: @name)
+  #   @festivals = Festival.where("name ILIKE ?", @name)
+  # end
 end
 
 def show
-  @festival = Festival.where(name: params[:search][:name])
+  @festival = Festival.find(params[:search][:id])
 end
 
 end
