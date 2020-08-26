e3e8a55c9400976fd2536d8f421b23d942b6e0ed
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-26 16:54:19 +0100

    dropdown showing different displays

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'find_festival', to: 'festivals#find_festival'
+  get 'display_friday', to: 'festivals#display_friday'
+  get 'display_saturday', to: 'festivals#display_saturday'
+  get 'display_sunday', to: 'festivals#display_sunday'
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
