b3592dce0658487bfd14cb024db5f24898519ff3
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-26 15:24:41 +0100

    some fixes in festivals

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'find_festival', to: 'festivals#find_festival'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :festival, only: [:index, :show]
+  resources :festivals, only: [:index, :show]
 end
