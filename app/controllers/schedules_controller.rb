e740e803ffa361274b09f9d26be8358845e5753c
Shaun Lennon <shaunlennon1992@gmail.com>
2020-08-26 12:13:24 +0100

    added routes, created my festipal view

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,36 +3,36 @@ class SchedulesController < ApplicationController
     @schedules = Schedule.where(festival_id: 1, day: 'Friday')
   end
 
-  # def create
-  #   @schedule = Schedule.new(strong_params)
-  #   @schedule.user = current_user
-  #   @festival = Festival.find(params[:festival_id])
-  #   @festival.schedule = @schedule
-  #   if @schedule.save!
-  #     redirect_to root_path # change this path
-  #   end
-  # end
+  def create
+    @schedule = Schedule.new(strong_params)
+    @schedule.user = current_user
+    @festival = Festival.find(params[:festival_id])
+    @festival.schedule = @schedule
+    if @schedule.save!
+      redirect_to myfestipal_path # change this path
+    end
+  end
 
-  # def update
-  #   find_by_id
-  #   if @schedule.update(strong_params)
-  #     redirect_to root_path # change this path
-  #   end
-  # end
+  def update
+    find_by_id
+    if @schedule.update(strong_params)
+      redirect_to myfestipal_path # change this path
+    end
+  end
 
-  # def destroy
-  #   find_by_id
-  #   @schedule.destroy
-  #   redirect_to root_path # change this path
-  # end
+  def destroy
+    find_by_id
+    @schedule.destroy
+    redirect_to myfestipal_path # change this path
+  end
 
-  # private
+  private
 
-  # def strong_params
-  #   params.require(:schedule).permit(:festival_id, :day, :start_time, :end_time, :artist, :genre, :stage)
-  # end
+  def strong_params
+    params.require(:schedule).permit(:festival_id, :day, :start_time, :end_time, :artist, :genre, :stage)
+  end
 
-  # def find_by_id
-  #   @schedule = Schedule.find(params[:id])
-  # end
+  def find_by_id
+    @schedule = Schedule.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'schedules/index'
-  get 'schedules/create'
-  get 'schedules/update'
-  get 'schedules/destroy'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  get 'myfestipal', to: 'schedules#myfestipal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :festival, only: [:index, :show] do
     resource :schedule, only: [:index, :create, :update, :destroy]
