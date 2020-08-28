ca8135d076346a973cde8a5e17f63ab00a1686af
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-28 12:16:11 +0100

    alert working, needs styling

--- app/javascript/custom/lineup_disable.js
+++ app/javascript/custom/lineup_disable.js
@@ -2,6 +2,7 @@
 const disableLink = () => {
   console.log("calling the function");
 
+
   const buttons = document.querySelectorAll('.dropdown-button');
 
 
@@ -9,13 +10,18 @@ const disableLink = () => {
     buttons.forEach((button) => {
       button.addEventListener("click", () => {
         setTimeout(function(){
+
+          const alert = document.getElementById('show-alert');
+          console.log(alert);
+
           const links = document.querySelectorAll('.lineup-link');
           console.log(links);
           links.forEach(function(link){
             console.log('inside the forEach');
           // console.log(link);
             link.addEventListener("click", () => {
-              link.classList.add('disabled-lineup');
+            link.classList.add('disabled-lineup');
+            showAlert(alert);
             });
           });
         }, 1000);
@@ -23,7 +29,15 @@ const disableLink = () => {
       });
     });
   };
+  const showAlert = (alert) => {
+    alert.removeAttribute('hidden');
+    setTimeout(function(){
+      console.log(alert);
+      alert.setAttribute('hidden', false);
+    }, 2000);
+  };
 };
+
 
 export { disableLink };
 
