71f98e6291e610086e0c42e26eace346b5f06b40
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-09-02 14:49:59 +0100

    cross working in unselected slots

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -14,25 +14,25 @@ class FestivalsController < ApplicationController
 
   def display_friday
     @festival = Festival.find(params[:festival])
-    @festivalschedules = @festival.schedules.where(day: 'Friday')
-    @userschedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Friday' }
-    @start_times = (@festivalschedules & @userschedules).collect(&:start_time)
+    @festival_schedules_day = @festival.schedules.where(day: 'Friday')
+    @user_schedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Friday' }
+    @start_times_day = (@festival_schedules_day & @user_schedules).collect(&:start_time)
     render :display_friday
   end
 
   def display_saturday
     @festival = Festival.find(params[:festival])
-    @festivalschedules = @festival.schedules.where(day: 'Saturday')
-    @userschedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Saturday' }
-    @start_times = (@festivalschedules & @userschedules).collect(&:start_time)
+    @festival_schedules_day = @festival.schedules.where(day: 'Saturday')
+    @user_schedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Saturday' }
+    @start_times_day = (@festival_schedules_day & @user_schedules).collect(&:start_time)
     render :display_saturday
   end
 
   def display_sunday
     @festival = Festival.find(params[:festival])
-    @festivalschedules = @festival.schedules.where(day: 'Sunday')
-    @userschedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Sunday' }
-    @start_times = (@festivalschedules & @userschedules).collect(&:start_time)
+    @festival_schedules_day = @festival.schedules.where(day: 'Sunday')
+    @user_schedules = UserSchedule.where(user: current_user).collect(&:schedule).filter { |x| x.day == 'Sunday' }
+    @start_times_day = (@festival_schedules_day & @user_schedules).collect(&:start_time)
     render :display_sunday
   end
 end
